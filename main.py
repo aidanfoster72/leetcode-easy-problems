8b062e350a0a9f65d9d2adc316d2dbf8833e0a9c
Foster <W0458487@campus.nscc.ca>
2025-08-19 14:21:38 -0300

    Leetcode more problems

--- main.py
+++ main.py
@@ -337,3 +337,185 @@ class UndergroundSystem(object):
 # obj.checkIn(10,"Leyton",24)
 # print(obj.getAverageTime("Leyton","Waterloo"))
 
+
+
+
+# 2. add two numbers
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            carry, total = divmod(val1 + val2 + carry, 10)
+            current.next = ListNode(total)
+            current = current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+
+
+# l1 = build_linked_list([2, 4, 3])
+# l2 = build_linked_list([5, 6, 4])
+# res1 = Solution().addTwoNumbers(l1, l2)
+# print_linked_list(res1)
+
+# l3 = build_linked_list([9, 9, 9, 9, 9, 9, 9])
+# l4 = build_linked_list([9, 9, 9, 9])
+# res2 = Solution().addTwoNumbers(l3, l4)
+# print_linked_list(res2)
+
+# 3. longest substring without repeating characters
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        char_set = set()
+        left = 0
+        max_length = 0
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        return max_length
+
+# print(Solution().lengthOfLongestSubstring("abcabcbb"))
+# print(Solution().lengthOfLongestSubstring("bbbbb"))
+# print(Solution().lengthOfLongestSubstring("pwwkew"))
+# print(Solution().lengthOfLongestSubstring(""))
+# print(Solution().lengthOfLongestSubstring(" "))
+# print(Solution().lengthOfLongestSubstring("au"))
+# print(Solution().lengthOfLongestSubstring("dvdf"))
+# print(Solution().lengthOfLongestSubstring("abba"))
+# print(Solution().lengthOfLongestSubstring("abcabcbb"))
+
+# 4. median of two sorted arrays
+
+# class Solution(object):
+#     def findMedianSortedArrays(self, nums1, nums2):
+#         nums1.extend(nums2)
+#         nums1.sort()
+#         print(nums1)
+#         print(len(nums1))
+#         if len(nums1) % 2 == 0:
+#             return float(nums1[len(nums1)//2 - 1] + nums1[len(nums1)//2]) / 2
+#         else:
+#             return float(nums1[len(nums1)//2])
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Ensure nums1 is the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+
+            if i < m and nums2[j-1] > nums1[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and nums1[i-1] > nums2[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+                if i == 0: max_of_left = nums2[j-1]
+                elif j == 0: max_of_left = nums1[i-1]
+                else: max_of_left = max(nums1[i-1], nums2[j-1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left  # Odd length
+
+                if i == m: min_of_right = nums2[j]
+                elif j == n: min_of_right = nums1[i]
+                else: min_of_right = min(nums1[i], nums2[j])
+
+                return (max_of_left + min_of_right) / 2.0
+        
+# print(Solution().findMedianSortedArrays([1,2],[3,4]))
+
+
+# 5. longest palindromic substring
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            len1 = self.expandAroundCenter(s, i, i)
+            len2 = self.expandAroundCenter(s, i, i+1)
+            max_len = max(len1, len2)
+            if max_len > end - start:
+                start = i - (max_len - 1) // 2
+                end = i + max_len // 2
+        return s[start:end+1]
+        
+    
+    def expandAroundCenter(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+
+# print(Solution().longestPalindrome("babad"))
+# print(Solution().longestPalindrome("cbbd"))   
+# print(Solution().longestPalindrome("a"))
+
+
+
+#  6. zigzag conversion
+
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+        rows = [""] * numRows
+        index, step = 0, 1
+        for char in s:
+            rows[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        return "".join(rows)
+
+# print(Solution().convert("PAYPALISHIRING", 3))
+# print(Solution().convert("PAYPALISHIRING", 4))
+# print(Solution().convert("A", 1))
+
+
+#  7. reverse integer
+
+class Solution(object):
+    def reverse(self, x):
+        x = str(x)
+        if x[0] == "-":
+            x = x[1:]
+            x = x[::-1]
+            x = "-" + x
+        else:
+            x = x[::-1]
+        if int(x) > 2**31 - 1 or int(x) < -2**31:
+            return 0
+        return int(x)
+
+print(Solution().reverse(123))
+print(Solution().reverse(-123))
+print(Solution().reverse(120))
+print(Solution().reverse(0))
+print(Solution().reverse(1534236469))
